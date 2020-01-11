827670b3d2a31fb62951a49c6ac0fda78b6b9fe0
bigsk <yuncraft@163.com>
2020-01-11 20:24:02 +0800

    上传批量生成程序

---
+++ 网站用批量生成/生成器/redo.py
@@ -0,0 +1,13 @@
+import os
+
+os.system('del ..\ARAGS\*.png /s/q')
+for i in range(1,1001):
+    os.system('py Ray.py')
+for i in range(1,1001):
+	os.system('py King.py')
+for i in range(1,1001):
+	os.system('py Anmicius.py')
+for i in range(1,1001):
+	os.system('py Camil.py')
+for i in range(1,1001):
+	os.system('py Grey.py')
