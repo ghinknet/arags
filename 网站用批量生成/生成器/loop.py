31df1cb442f7acea9f3e6ff79b65899be751fa3f
Bigsk <yuncraft@163.com>
2020-02-16 14:06:49 +0800

    更新嘉德罗斯生成器

---
+++ 网站用批量生成/生成器/loop.py
@@ -0,0 +1,15 @@
+import os
+
+os.system('del ..\ARAGS\*.png /s/q')
+for i in range(1,1001):
+    os.system('py Ray.py')
+for i in range(1,1001):
+	os.system('py King.py')
+for i in range(1,1001):
+	os.system('py Anmicius.py')
+for i in range(1,1001):
+	os.system('py Camil.py')
+for i in range(1,1001):
+	os.system('py Grey.py')
+for i in range(1,1001):
+	os.system('py Godrose.py')
